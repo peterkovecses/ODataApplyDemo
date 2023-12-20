c7319b17bf62316c615b468e0a5f14f5e8660ebc
Peter Kovecses <Peter_Kovecses@epam.com>
2023-12-20 17:33:21 +0100

    Refactor middleware

--- ODataApplyDemo/Middlewares/ODataApplyPatchMiddleware.cs
+++ ODataApplyDemo/Middlewares/ODataApplyPatchMiddleware.cs
@@ -11,41 +11,51 @@ public class ODataApplyPatchMiddleware(RequestDelegate next)
     {
         var response = context.Response;
         var originalResponseBodyStream = response.Body;
-        using var newResponseBody = new MemoryStream();
-        response.Body = newResponseBody;
+        using var updatedBodyStream = new MemoryStream();
+        response.Body = updatedBodyStream;
 
         await next(context);
 
         if (context.Response.Headers.TryGetValue(HeaderKeys.ODataApplyPatch, out _))
         {
             context.Response.Headers.Remove(HeaderKeys.ODataApplyPatch);
-            await ModifyResponseAsync(response, newResponseBody, context.Request.FullUrl());
+            await UpdateResponseBodyAsync(response, updatedBodyStream , context.Request.FullUrl());
         }
 
-        newResponseBody.Seek(0, SeekOrigin.Begin);
-        await newResponseBody.CopyToAsync(originalResponseBodyStream);
-        response.Body = originalResponseBodyStream;
+        await FinalizeResponseBody(updatedBodyStream, originalResponseBodyStream, response);
     }
 
-    private async Task ModifyResponseAsync(HttpResponse response, MemoryStream newBody, string fullUrl)
+    private static async Task UpdateResponseBodyAsync(HttpResponse response, Stream updatedBodyStream, string fullUrl)
     {
         var stream = response.Body;
-        using var reader = new StreamReader(stream, leaveOpen: true);
-        var originalResponse = await reader.ReadToEndAsync();
-        newBody.Seek(0, SeekOrigin.Begin);
-        var responseBody = new StreamReader(newBody).ReadToEnd();
-        newBody.Seek(0, SeekOrigin.Begin);
-        var value = JsonSerializer.Deserialize<object[]>(responseBody);
-        var odataResponse = new OdataResponseWrapper
-        {
-            Context = fullUrl,
-            Value = value!
-        };
-        var jsonContent = JsonSerializer.Serialize(odataResponse);
+        updatedBodyStream.Seek(0, SeekOrigin.Begin);
+        var responseBody = await new StreamReader(updatedBodyStream).ReadToEndAsync();
+        updatedBodyStream.Seek(0, SeekOrigin.Begin);
+        var jsonContent = GenerateODataResponseContent(fullUrl, responseBody);
         stream.SetLength(0);
         using var writer = new StreamWriter(stream, leaveOpen: true);
         await writer.WriteAsync(jsonContent);
         await writer.FlushAsync();
         response.ContentLength = stream.Length;
+    }
+
+    private static string GenerateODataResponseContent(string requestUrl, string responseBody)
+    {
+        var value = JsonSerializer.Deserialize<object[]>(responseBody);
+        var odataResponse = new OdataResponseWrapper
+        {
+            Context = requestUrl,
+            Value = value!
+        };
+        var jsonContent = JsonSerializer.Serialize(odataResponse);
+        return jsonContent;
+    }
+
+    private static async Task FinalizeResponseBody(Stream updatedBodyStream, Stream originalResponseBodyStream,
+        HttpResponse response)
+    {
+        updatedBodyStream.Seek(0, SeekOrigin.Begin);
+        await updatedBodyStream.CopyToAsync(originalResponseBodyStream);
+        response.Body = originalResponseBodyStream;
     }
 }
